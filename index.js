b98671f20c63d96944a5dec05a24029d3fd8a86f
ronescl <rones_cley@hotmail.com>
2021-11-09 22:46:48 -0300

    frist commit

---
+++ index.js
@@ -0,0 +1,94 @@
+//Exercicio 1
+
+ //let letra a = 10
+//let letra b = 10
+//o valor de a = 10 e o valor de b=10
+
+//console.log (b)
+//b = 5 
+//console.log (10,5)
+
+//exercicio 2
+
+//console.log
+//let a = 10
+//let b = 20
+//c = a
+//b = c
+//a = b
+
+//se c=a, então c é 10
+//se b=c, então b agora é =10
+//se a=b, então a continua sendo 10
+
+//console.log (a=10, b=10, c=10)
+
+//exercicio 3
+
+/*
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+let (Seria melhor que as variaveis "p" e "t" tivessem descrições mais adequadas como: 
+horasTrabalhadasNoDia/ valorRecebido)
+*/
+
+
+//Exercício de escrita de código
+/*A
+const nomeDaPessoa = 
+B
+const idadeDaPessoa =
+C
+console.log("o nome dela (e) é", nome, "e tem", idade, "anos")
+
+let nome 
+let idade
+
+console.log(typeof nome)
+console.log(typeof idade)
+D
+//Retornou undefined, pois eu não defini o nome nem a idade
+
+E 
+console.log (prompt("Qual é o seu nome?"))
+console.log (prompt("Qual é a sua idade"))
+
+F 
+
+
+G 
+console.log (prompt ("Ola", nome, "você tem", idade, "anos"))
+
+
+2
+const vocêEstáUsandoUmaRoupaAzulHoje = true
+const voceTorceParaOCruzeiro = true
+const voceTem20Anos = false
+
+console.log (prompt("Você está usando uma roupa azul hoje?", "sim"))
+console.log (prompt("voce Torce Para O Cruzeiro", "sim"))
+console.log (prompt("voce tem 20 anos", "não"))
+
+B
+let a = 10
+let b = 25
+let c = -10
+c = a
+b = c
+a = b
+
+console.log("O novo valor de a é", a) // O novo valor de a é 25
+console.log("O novo valor de b é", b) // O novo valor de b é 10
+*/
+
+
+
+
+
+
+
+
+
+
+
+
